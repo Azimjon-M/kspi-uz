125502644cc39a03f5ce1df6898149a979698752
nodirbek27 <nodirjon0927@gmail.com>
2023-12-12 15:41:13 +0500

    add news

---
+++ src/components/News/index.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const News = () => {
+    return (
+        <div>
+
+            {/* items */}
+            <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains" />
+                <div class="px-6 py-4">
+                    <h2 class="font-bold text-xl mb-2">The Coldest Sunset</h2>
+                </div>
+                <div class="px-6 pt-4 pb-2 flex justify-between items-center">
+                    <div class="inline-block mr-2 mb-2">12.12.2023</div>
+                    <button className='btn bg-[#004269] text-white hover:border-[#004269] hover:bg-[#fff]'>Batafsil</button>
+                </div>
+            </div>
+
+            {/* items */}
+            <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains" />
+                <div class="px-6 py-4">
+                    <h2 class="font-bold text-xl mb-2">The Coldest Sunset</h2>
+                </div>
+                <div class="px-6 pt-4 pb-2 flex justify-between items-center">
+                    <div class="inline-block mr-2 mb-2">12.12.2023</div>
+                    <button className='btn bg-[#004269] text-white hover:border-[#004269] hover:bg-[#fff]'>Batafsil</button>
+                </div>
+            </div>
+
+            {/* items */}
+            <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains" />
+                <div class="px-6 py-4">
+                    <h2 class="font-bold text-xl mb-2">The Coldest Sunset</h2>
+                </div>
+                <div class="px-6 pt-4 pb-2 flex justify-between items-center">
+                    <div class="inline-block mr-2 mb-2">12.12.2023</div>
+                    <button className='btn bg-[#004269] text-white hover:border-[#004269] hover:bg-[#fff]'>Batafsil</button>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default News;
+
+
--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
 import Carousel from '../../components/Carousel';
+import News from '../../components/News';
 
 function Main() {
     return (
         <div>
             <Navbar />
             <Carousel />
+            <News />
         </div>
     )
 }
