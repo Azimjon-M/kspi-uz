ffbc9cd24e12368c129174b3ad412240bc94d45a
Azimjon <120557486+Azimjon-M@users.noreply.github.com>
2023-12-12 16:31:49 +0500

    Navbar updere response

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -5,14 +5,18 @@ import { Link } from "react-router-dom";
 import { AiOutlineSearch, AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import { useFormik } from "formik";
 import { useDispatch, useSelector } from "react-redux";
-import {setLangUz, setLangRu, setLangEn} from "../../redux/moduls/language/action/";
+import {
+    setLangUz,
+    setLangRu,
+    setLangEn,
+} from "../../redux/moduls/language/action/";
 
 import flag_1 from "../../assets/icons/flag-uz.png";
 import flag_2 from "../../assets/icons/flag-ru.png";
 import flag_3 from "../../assets/icons/flag-en.png";
 
 function Navbar() {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
     const isLang = useSelector((state) => state.reducerLang.isLang);
 
     const [isFocusedSearInp, setFocusedSearInp] = useState(false);
@@ -55,23 +59,23 @@ function Navbar() {
     };
 
     return (
-        <nav className="flex justify-between bg-white shadow-2xl sticky z-50 px-8 py-4">
+        <nav className="flex justify-between bg-white shadow-2xl sticky z-50 px-4 py-4 xl:px-0 xl:py-0">
             {/* Doimo bor */}
             <Link to="/">
-                <div className="w-[150px] lg:min-w-[250px] flex items-center gap-x-[10px] lg:my-[25px] lg:mx-[60px]">
+                <div className="w-[150px] flex items-center gap-x-[10px] xl:w-[280px] xl:my-[25px] xl:ms-[40px]">
                     <img
-                        className="w-[35px] lg:w-[60px] h-auto"
+                        className="w-[32px] xl:w-[60px] h-auto"
                         src={kspi_logo}
                         alt="icon"
                     />
-                    <p className="text-[14px] leading-4 lg:text-[18px] text-[#004269] font-bold">
+                    <p className="text-[11px] leading-4 text-[#004269] font-bold xl:leading-6 xl:text-[16px]">
                         <FormattedMessage id="navLogo" />
                     </p>
                 </div>
             </Link>
             {/* lg dan bor */}
             <div className="w-full flex items-end justify-center flex-col">
-                <div className="hidden lg:flex lg:justify-between bg-[#004269] text-white px-[20px] rounded-bl-lg ">
+                <div className="hidden xl:flex xl:justify-between bg-[#004269] text-white px-[20px] rounded-bl-lg ">
                     <ul className="flex gap-x-[20px] text-[15px] font-medium">
                         <li className="py-[4px]">
                             <Link
@@ -82,8 +86,26 @@ function Navbar() {
                             </Link>
                         </li>
                         <li className="cursor-pointer py-[4px]">
-                            {/* Dropdown Boshqa saytlarga Yo'llantiriladi */}
-                            <FormattedMessage id="hedInstitut-jurnali" />
+                            <div className="dropdown dropdown-hover">
+                                <div
+                                    tabIndex={0}
+                                    role="button"
+                                    className="text-inherit"
+                                >
+                                    <FormattedMessage id="hedInstitut-jurnali" />
+                                </div>
+                                <ul
+                                    tabIndex={0}
+                                    className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 text-black"
+                                >
+                                    <li>
+                                        <a>Item 1</a>
+                                    </li>
+                                    <li>
+                                        <a>Item 2</a>
+                                    </li>
+                                </ul>
+                            </div>
                         </li>
                         <li className="py-[4px]">
                             <Link
@@ -114,7 +136,7 @@ function Navbar() {
                                 <input
                                     className={`${
                                         isFocusedSearInp
-                                            ? "w-[200px] border-b-4 border-blue-400"
+                                            ? "w-[200px] border-b-[3px] border-white ms-4"
                                             : "w-[0px] -z-50"
                                     } style-transition-01 h-full focus:outline-none bg-inherit px-2`}
                                     placeholder="text..."
@@ -145,43 +167,166 @@ function Navbar() {
                         </form>
                     </div>
                 </div>
-                <div className="hidden w-full h-full lg:flex lg:items-center lg:justify-center">
-                    <ul className="flex items-center gap-x-6 text-[22px] text-[#004269] font-semibold">
+                <div className="hidden w-full h-full xl:flex xl:items-center xl:justify-end px-10">
+                    <ul className="flex items-center gap-x-4 text-[18px] text-[#004269] font-semibold">
                         <li>
-                            <FormattedMessage id="navYangiliklar" />
+                            <div className="dropdown dropdown-hover">
+                                <div
+                                    tabIndex={1}
+                                    role="button"
+                                    className="text-inherit"
+                                >
+                                    <FormattedMessage id="navYangiliklar" />
+                                </div>
+                                <ul
+                                    tabIndex={1}
+                                    className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+                                >
+                                    <li>
+                                        <a>Item 1</a>
+                                    </li>
+                                    <li>
+                                        <a>Item 2</a>
+                                    </li>
+                                </ul>
+                            </div>
                         </li>
                         <li>
-                            <FormattedMessage id="navInstitut" />
+                            <div className="dropdown dropdown-hover">
+                                <div
+                                    tabIndex={2}
+                                    role="button"
+                                    className="text-inherit"
+                                >
+                                    <FormattedMessage id="navInstitut" />
+                                </div>
+                                <ul
+                                    tabIndex={2}
+                                    className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+                                >
+                                    <li>
+                                        <a>Item 1</a>
+                                    </li>
+                                    <li>
+                                        <a>Item 2</a>
+                                    </li>
+                                </ul>
+                            </div>
                         </li>
                         <li>
-                            <FormattedMessage id="navFaoliyat" />
+                            <div className="dropdown dropdown-hover">
+                                <div
+                                    tabIndex={3}
+                                    role="button"
+                                    className="text-inherit"
+                                >
+                                    <FormattedMessage id="navFaoliyat" />
+                                </div>
+                                <ul
+                                    tabIndex={3}
+                                    className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+                                >
+                                    <li>
+                                        <a>Item 1</a>
+                                    </li>
+                                    <li>
+                                        <a>Item 2</a>
+                                    </li>
+                                </ul>
+                            </div>
                         </li>
                         <li>
-                            <FormattedMessage id="navTuzilma" />
+                            <div className="dropdown dropdown-hover">
+                                <div
+                                    tabIndex={4}
+                                    role="button"
+                                    className="text-inherit"
+                                >
+                                    <FormattedMessage id="navTuzilma" />
+                                </div>
+                                <ul
+                                    tabIndex={4}
+                                    className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+                                >
+                                    <li>
+                                        <a>Item 1</a>
+                                    </li>
+                                    <li>
+                                        <a>Item 2</a>
+                                    </li>
+                                </ul>
+                            </div>
                         </li>
                         <li>
-                            <FormattedMessage id="navTalabalar" />
+                            <div className="dropdown dropdown-hover">
+                                <div
+                                    tabIndex={5}
+                                    role="button"
+                                    className="text-inherit"
+                                >
+                                    <FormattedMessage id="navTalabalar" />
+                                </div>
+                                <ul
+                                    tabIndex={5}
+                                    className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+                                >
+                                    <li>
+                                        <a>Item 1</a>
+                                    </li>
+                                    <li>
+                                        <a>Item 2</a>
+                                    </li>
+                                </ul>
+                            </div>
                         </li>
                         <li>
-                            <FormattedMessage id="navAbiturient" />
+                            <div className="dropdown dropdown-hover">
+                                <div
+                                    tabIndex={6}
+                                    role="button"
+                                    className="text-inherit"
+                                >
+                                    <FormattedMessage id="navAbiturient" />
+                                </div>
+                                <ul
+                                    tabIndex={6}
+                                    className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+                                >
+                                    <li>
+                                        <a>Item 1</a>
+                                    </li>
+                                    <li>
+                                        <a>Item 2</a>
+                                    </li>
+                                </ul>
+                            </div>
                         </li>
                         <li>
-                            <div className="flex items-end gap-x-2">
+                            <div className="flex items-end gap-x-2 ms-8">
                                 <img
                                     onClick={() => handleClickLang("uz")}
-                                    className={` ${isLang === "uz" && 'border-b-[3px] border-[#004269]'} w-[35px] cursor-pointer`}
+                                    className={` ${
+                                        isLang === "uz" &&
+                                        "border-b-[3px] border-[#004269]"
+                                    } w-[30px] cursor-pointer`}
                                     src={flag_1}
                                     alt="flag uz"
                                 />
                                 <img
                                     onClick={() => handleClickLang("ru")}
-                                    className={` ${isLang === "ru" && 'border-b-[3px] border-[#004269]'} w-[35px] cursor-pointer`}
+                                    className={` ${
+                                        isLang === "ru" &&
+                                        "border-b-[3px] border-[#004269]"
+                                    } w-[30px] cursor-pointer`}
                                     src={flag_2}
                                     alt="flag ru"
                                 />
                                 <img
                                     onClick={() => handleClickLang("en")}
-                                    className={` ${isLang === "en" && 'border-b-[3px] border-[#004269]'} w-[35px] cursor-pointer`}
+                                    className={` ${
+                                        isLang === "en" &&
+                                        "border-b-[3px] border-[#004269]"
+                                    } w-[30px] cursor-pointer`}
                                     src={flag_3}
                                     alt="flag en"
                                 />
@@ -190,7 +335,7 @@ function Navbar() {
                     </ul>
                 </div>
                 {/* lg ga qadar bor */}
-                <button className="btn btn-outline lg:hidden flex items-center gap-x-2 font-medium">
+                <button className="btn btn-outline xl:hidden flex items-center gap-x-2 font-medium">
                     <AiOutlineMenu />
                     MENU
                 </button>
