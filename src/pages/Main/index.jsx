afca9f8bc5273a46dcd7ca12ff252d3bb3686d45
Orifxon3595 <orifxontuychiyev@gmail.com>
2023-12-13 09:06:10 +0500

    Interactive add

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
 import Carousel from '../../components/Carousel';
+import Interactive from '../../components/Interactive';
 
 function Main() {
     return (
         <div>
             <Navbar />
             <Carousel />
+            <Interactive />
         </div>
     )
 }
