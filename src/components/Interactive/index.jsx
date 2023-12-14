aad72b662cd8e5fb68cc4eea6ee35d7e9d2e8f16
Orifxon3595 <orifxontuychiyev@gmail.com>
2023-12-14 09:30:12 +0500

    Inter active ++

--- src/components/Interactive/index.jsx
+++ src/components/Interactive/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { RiComputerLine } from "react-icons/ri";
 import { GiBookshelf } from "react-icons/gi";
+import { MdOutlineLiveTv } from "react-icons/md";
+import { PiChalkboardTeacherFill } from "react-icons/pi";
 
 function Interactive() {
   return (
@@ -26,15 +28,15 @@ function Interactive() {
           </li>
           <li className=" flex justify-center w-2/12">
             <Link className="flex flex-col justify-center items-center group my-9 w-full border-r border-[#ebebeb] text-center">
-              <RiComputerLine className="text-[50px] text-[#a3a1a2] group-hover:scale-125 group-hover:text-[#004269] transition-all duration-75 ease-in-out" />
+              <PiChalkboardTeacherFill className="text-[50px] text-[#a3a1a2] group-hover:scale-125 group-hover:text-[#004269] transition-all duration-75 ease-in-out" />
               <h2 className="uppercase mt-2 text-[#004269] font-bold">
-                Virtual qabulxona
+                Masofaviy ta'lim
               </h2>
             </Link>
           </li>
           <li className=" flex justify-center w-2/12">
             <Link className="flex flex-col justify-center items-center group my-9 w-full border-r border-[#ebebeb] text-center">
-              <RiComputerLine className="text-[50px] text-[#a3a1a2] group-hover:scale-125 group-hover:text-[#004269] transition-all duration-75 ease-in-out" />
+              <MdOutlineLiveTv className="text-[50px] text-[#a3a1a2] group-hover:scale-125 group-hover:text-[#004269] transition-all duration-75 ease-in-out" />
               <h2 className="uppercase mt-2 text-[#004269] font-bold">
                 Virtual qabulxona
               </h2>
