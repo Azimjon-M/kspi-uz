e4f17ddf2a86706ae963c1f6ec11dff857c40c1f
Orifxon3595 <orifxontuychiyev@gmail.com>
2023-12-14 09:55:05 +0500

    Interactive add

--- src/components/Interactive/index.jsx
+++ src/components/Interactive/index.jsx
@@ -4,17 +4,18 @@ import { RiComputerLine } from "react-icons/ri";
 import { GiBookshelf } from "react-icons/gi";
 import { MdOutlineLiveTv } from "react-icons/md";
 import { PiChalkboardTeacherFill } from "react-icons/pi";
+import { FormattedMessage } from "react-intl";
 
 function Interactive() {
   return (
     <div>
-      <div className="max-w-6xl mx-auto relative -top-5 z-50 bg-white rounded-lg shadow-md">
+      <div className="max-w-7xl mx-auto relative -top-5 z-50 bg-white rounded-lg shadow-md">
         <ul className="flex">
           <li className=" flex justify-center w-2/12">
             <Link className="flex flex-col justify-center items-center group my-9 w-full border-r border-[#ebebeb] text-center">
               <RiComputerLine className="text-[50px] text-[#a3a1a2] group-hover:scale-125 group-hover:text-[#004269] transition-all duration-75 ease-in-out" />
               <h2 className="uppercase mt-2 text-[#004269] font-bold">
-                Virtual qabulxona
+                <FormattedMessage id="intVertualQabulxona" />
               </h2>
             </Link>
           </li>
@@ -38,7 +39,7 @@ function Interactive() {
             <Link className="flex flex-col justify-center items-center group my-9 w-full border-r border-[#ebebeb] text-center">
               <MdOutlineLiveTv className="text-[50px] text-[#a3a1a2] group-hover:scale-125 group-hover:text-[#004269] transition-all duration-75 ease-in-out" />
               <h2 className="uppercase mt-2 text-[#004269] font-bold">
-                Virtual qabulxona
+                Video maruzalar
               </h2>
             </Link>
           </li>
@@ -46,7 +47,7 @@ function Interactive() {
             <Link className="flex flex-col justify-center items-center group my-9 w-full border-r border-[#ebebeb] text-center">
               <RiComputerLine className="text-[50px] text-[#a3a1a2] group-hover:scale-125 group-hover:text-[#004269] transition-all duration-75 ease-in-out" />
               <h2 className="uppercase mt-2 text-[#004269] font-bold">
-                Virtual qabulxona
+                Qabul-2024
               </h2>
             </Link>
           </li>
@@ -54,7 +55,7 @@ function Interactive() {
             <Link className="flex flex-col justify-center items-center group my-9 w-full text-center">
               <RiComputerLine className="text-[50px] text-[#a3a1a2] group-hover:scale-125 group-hover:text-[#004269] transition-all duration-75 ease-in-out" />
               <h2 className="uppercase mt-2 text-[#004269] font-bold">
-                Virtual qabulxona
+                Imtixonlarni kuzatish
               </h2>
             </Link>
           </li>
