1820387313d72eaeddd58bf8d58bb446dc54d800
nodirbek27 <nodirjon0927@gmail.com>
2024-02-05 14:09:12 +0500

    update interaktiv++

--- src/components/VideoMaruzaCom/index.jsx
+++ src/components/VideoMaruzaCom/index.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const VideoMaruzalar = () => {
   return (
-    <div>videoMaruzalar</div>
-  )
-}
+    <div>
+      <h2 className="text-center text-2xl my-5 font-bold">Video maruzalar</h2>
+    </div>
+  );
+};
 
 export default VideoMaruzalar;
