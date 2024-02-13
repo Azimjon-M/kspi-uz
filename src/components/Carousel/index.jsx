7319cf877d4114c736efb1da7327c1e1d88676d5
nodirbek27 <nodirjon0927@gmail.com>
2024-02-13 15:46:54 +0500

    carousel++

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -22,32 +22,17 @@ const Carousel = () => {
       }}
       loop={true}
       modules={[Autoplay, Navigation]}
-      className="mySwiper w-full h-[60vh] md:h-[80vh] transition-all duration-2000 ease-in-out"
+      className="mySwiper w-auto h-[80vh] object-cover md:h-[90vh] transition-all duration-2000 ease-in-out"
     >
       {images.map((img, index) => (
         <SwiperSlide key={index}>
           <div className="relative">
             <img
-              className="block w-full h-full object-cover transition-all duration-3000 ease-linear"
+              className="block w-auto h-[100%] object-cover transition-all duration-3000 ease-linear"
               src={img}
               alt="Institut rasmi"
             />
-            <div className="absolute inset-0 bg-[#004269] opacity-50" />
-            <div className="absolute top-[50%] 2xl:top-[40%] -translate-y-[50%] xl:-translate-y-[180%] left-0 right-0 text-center max-w-sm md:max-w-md xl:max-w-xl 2xl:max-w-2xl mx-auto">
-              <h2 className="text-sm md:text-lg xl:text-xl 2xl:text-3xl font-semibold text-white mb-3">
-                Slide Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                {index + 1}
-              </h2>
-              <p className="mb-3 text-white ">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
-                incidunt ex ea, inventore necessitatibus unde. In nostrum
-                consectetur libero impedit odio corrupti dolorum, quos soluta,
-                eum culpa doloremque magnam optio!
-              </p>
-              <Link className="px-8 py-2 bg-[#004269] text-white xl:text-xl font-bold rounded-full shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50">
-                Link {index + 1}
-              </Link>
-            </div>
+            {/* <div className="absolute inset-0 bg-[#004269] opacity-50" /> */}
           </div>
         </SwiperSlide>
       ))}
