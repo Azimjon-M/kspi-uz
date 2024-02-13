3ad4dc02d68fe212f0f6de048fddd01388f81b4d
nodirbek27 <nodirjon0927@gmail.com>
2024-02-13 14:22:19 +0500

    footer++

--- src/components/NewsCard/index.jsx
+++ src/components/NewsCard/index.jsx
@@ -43,7 +43,7 @@ const NewsCard = () => {
         />
       </div>
 
-      <div className="p-10 xl:px-0 grid grid-cols-4 gap-10">
+      <div className="p-10 xl:px-0 md:grid md:grid-cols-1 lg:grid-cols-4 gap-10">
         <div className="col-span-3 ...">
           <h2 className="text-xl text-[#004269] xl:text-2xl font-bold text-center mb-5">
             {newsData.title}
@@ -56,48 +56,7 @@ const NewsCard = () => {
             <span>{newsData && newsData.body && newsData.body.slice(1)}</span>
           </div>
         </div>
-        <div className="...">
-          {news &&
-            news.map((item, idx) => (
-              <Link to={`/yangiliklar/${item.id}`} key={idx}>
-                {item && (
-                  <div className="group/item hover:cursor-pointer">
-                    <div className="flex rounded-lg dark:bg-gray-800 shadow-md hover:shadow-lg flex-col group/edit">
-                      <div className="flex items-center mb-3 relative overflow-hidden">
-                        <img
-                          className="w-full rounded group-hover/item:scale-105 ease-in duration-300 ..."
-                          src={item.rasm}
-                          alt="Sunset in the mountains"
-                        />
-                        <div className="absolute top-0 left-3 h-12 w-12 bg-[#802323] text-center flex flex-col text-sm p-1 rounded-b-md">
-                          {item.sana && (
-                            <span className="text-white">
-                              {item.sana.slice(8, 10)}.{item.sana.slice(5, 7)}
-                            </span>
-                          )}
-                          {item.sana && (
-                            <span className="text-white">
-                              {item.sana.slice(0, 4)}
-                            </span>
-                          )}
-                        </div>
-                        <div className="absolute bottom-0 border-[#004269] border-2 w-10 group-hover/edit:w-full ... ease-in duration-300 ..."></div>
-                      </div>
-                      {/* News title */}
-                      <div className="flex flex-col justify-between flex-grow px-2">
-                        <h2 className="leading-relaxed font-bold line-clamp-3 xl:line-clamp-2 text-base text-[#004269] text-center dark:text-gray-300 line">
-                          {item.title}
-                        </h2>
-                        <div className="flex justify-center items-center">
-                          <div className="border-4 bg-[#004269] w-10 my-5"></div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                )}
-              </Link>
-            ))}
-        </div>
+         
       </div>
     </div>
   );
