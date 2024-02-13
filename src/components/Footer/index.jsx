d62288dd8c329807f24500a0943b6c6dd5118a1c
nodirbek27 <nodirjon0927@gmail.com>
2024-02-13 16:30:56 +0500

    rekvizitlar++

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
         </ul>
       </div>
       {/* Biz bilan bog'lanish */}
-      <div className="flex justify-center md:justify-end mb-3 md:pr-10">
+      <div className="flex justify-center md:justify-end mb-3 md:pr-10 z-10">
         <Link
           target="blank"
           to={"https://t.me/kspi_official"}
@@ -54,7 +54,7 @@ const Footer = () => {
           <FaSquareFacebook className="h-6 lg:h-7 w-auto group-hover:bg-[#316FF6]" />
         </Link>
       </div>
-      <div className="md:grid md:grid-cols-2 lg:flex lg:justify-evenly lg:items-start px-5">
+      <div className="md:grid md:grid-cols-2 lg:flex lg:justify-evenly lg:items-start px-5 z-10">
         {/* Asosiy */}
         <div className="flex items-center mb-3">
           {/* about */}
--- src/components/RekvizitlarCom/index.jsx
+++ src/components/RekvizitlarCom/index.jsx
@@ -3,39 +3,58 @@ import Breadcrumb from "../Breadcrumb";
 
 const RekvizitlarCom = () => {
   return (
-    <div className="px-5 py-3 md:px-10 lg:px-20">
+    <div className="px-5 py-3 md:px-10 mx-auto max-w-7xl mt-10">
       <div className="border-b-2 border-[#004269] block w-full">
         <Breadcrumb
           steps={[{ text: "Bosh sahifa", link: "/" }, { text: "Rekvizitlar" }]}
         />
       </div>
-      <table className="mx-auto border-collapse border border-slate-400 ...">
+      <table
+        border="1"
+        className="table mx-auto"
+      >
         <thead>
-          <tr>
-            <th colSpan={2}>Qo’qon davlat pedagogika instuti</th>
+          <tr className="bg-[#ddd] text-center text-xl">
+            <th colSpan="2">Qo’qon davlat pedagogika instuti</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>INN:</td>
-            <td>200131171</td>
+            <td className="md:text-lg font-semibold">INN:</td>
+            <td className="md:text-lg font-semibold">200131171</td>
           </tr>
           <tr>
-            <td>OKONX:</td>
-            <td>92110</td>
+            <td className="md:text-lg font-semibold">OKONX:</td>
+            <td className="md:text-lg font-semibold">92110</td>
           </tr>
           <tr>
-            <td>Shxr:</td>
-            <td>100010860304057094100079001</td>
+            <td className="md:text-lg font-semibold">Shxr:</td>
+            <td className="md:text-lg font-semibold">
+              100010860304057094100079001
+            </td>
           </tr>
           <tr>
-            <td>
-              <p>Ro’yxatdan o’tkazilgan sana: 01.01.1931-yil</p>
-              <p>
+            <td colSpan="2">
+              <p className="md:text-lg font-semibold">Ro’yxatdan o’tkazilgan sana: 01.01.1931-yil</p>
+              <p className="md:text-lg font-semibold">
                 Manzil: Farg’ona viloyati, Qo’qon shahar, Turon ko’chasi 23 uy.
               </p>
-              <p>Tel: 73-542-38-38</p>
-              <p>Fax: 73-542-02-57</p>
+              <p className="md:text-lg font-semibold">Tel: 73-542-38-38</p>
+              <p className="md:text-lg font-semibold">Fax: 73-542-02-57</p>
+            </td>
+          </tr>
+          <tr className="bg-[#ddd] text-center text-xl">
+            <th colSpan="2">Bank rekvizitlari</th>
+          </tr>
+          <tr>
+            <td colSpan="2">
+              <p className="md:text-lg font-semibold">O’zR Moliya vazirligi G’aznachiligi</p>
+              <p className="md:text-lg font-semibold">h/r 23402000300100001010</p>
+              <p className="md:text-lg font-semibold">INN: 201122919</p>
+              <p className="md:text-lg font-semibold">Markaziy bank HKKM Toshkent shaxar boshqarmasi</p>
+              <p className="md:text-lg font-semibold">MFO 00014</p>
+              <p className="md:text-lg font-semibold">Qo’qon davlat pedagogika instituti</p>
+              <p className="md:text-lg font-semibold">INN: 200131171 IFUT: 85420</p>
             </td>
           </tr>
         </tbody>
