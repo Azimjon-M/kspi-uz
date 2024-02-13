b1ce0126d8812a86200a557e80c39d59f10e7319
Orifxon3595 <orifxontuychiyev@gmail.com>
2024-02-13 15:56:20 +0500

    partners update

---
+++ src/components/WarmThoughts/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function WarmThoughts() {
+  return (
+    <div>
+      <div>
+        <div>logo</div>
+        <h1 className="text-xl md:text-4xl">Ularning so'zlarida</h1>
+      </div>
+    </div>
+  );
+}
+
+export default WarmThoughts;
