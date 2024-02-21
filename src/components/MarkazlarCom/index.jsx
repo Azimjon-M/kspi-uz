5e920129a986036a4cea0552ca461b1aa22cb6e3
nodirbek27 <nodirjon0927@gmail.com>
2024-02-21 16:52:50 +0500

    tuzilma++

--- src/components/MarkazlarCom/index.jsx
+++ src/components/MarkazlarCom/index.jsx
@@ -177,29 +177,29 @@ const MarkazlarCom = () => {
           <div>
             <TabsBody className="p-0 lg:w-[600px] xl:w-[800px]">
               {filteredData.map(
-                ({ value, lavozim, img = "", ism, qabul, tel, link }) => (
+                ({ value, lavozim, img = "", ism, tel, link }) => (
                   <TabPanel key={value} value={value} className="py-0">
                     <div className="relative flex flex-col lg:flex-row mt-6 lg:mt-0 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl max-w-96 lg:max-w-full">
-                      <div className="relative h-56 lg:h-80 -mt-6 lg:my-auto lg:py-5 mx-4 overflow-hidden rounded-xl">
+                      <div className="relative -mt-6 px-3 lg:px-0 md:h-56 lg:h-auto lg:my-auto lg:overflow-hidden rounded-xl">
                         <img
                           src={img}
                           alt={lavozim}
-                          className="w-full lg:w-80 xl:w-96 h-full object-cover lg:rounded"
+                          className="w-full mb-3 lg:mb-0 h-56 lg:w-[320px] lg:h-[340px] xl:w-[300px] xl:h-[340px] object-cover rounded"
                         />
                       </div>
-                      <div className="flex flex-col justify-between py-3">
-                        <div className="p-3">
+                      <div className="flex flex-col justify-between xl:py-5 xl:mr-5">
+                        <div className="px-3">
                           <h3 className="block mb-2 font-sans text-lg xl:text-2xl antialiased font-semibold leading-snug tracking-normal text-[#004269] text-center lg:text-start">
                             {lavozim}
                           </h3>
                           <h4 className="block mb-2 font-sans text-lg xl:text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900 text-center lg:text-start">
                             {ism}
                           </h4>
-                          <h4 className="block mb-2 font-sans text-md antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+                          <h4 className="block mb-2 text-center lg:text-start font-sans text-md antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
                             Telefon: {tel}
                           </h4>
                         </div>
-                        <div className="p-3 pt-0 lg:place-items-end">
+                        <div className="p-3 pt-0 lg:place-items-end text-center lg:text-start">
                           <Link
                             to={link}
                             className="btn align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-[#004269] text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:bg-[#004269]/90 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
