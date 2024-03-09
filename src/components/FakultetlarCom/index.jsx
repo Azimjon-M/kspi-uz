f2a22bac474557e8cf40434726752ef0c293d59d
nodirbek27 <nodirjon0927@gmail.com>
2024-03-09 14:09:46 +0500

    tuzilma++

--- src/components/FakultetlarCom/index.jsx
+++ src/components/FakultetlarCom/index.jsx
@@ -6,10 +6,17 @@ import {
   TabPanel,
 } from "@material-tailwind/react";
 import Breadcrumb from "../Breadcrumb";
-import raqamliImg from "../../assets/images/raqamli.JPG";
-import oquv from "../../assets/images/oquv.jpg";
-import muhandis from "../../assets/images/muhandis.jpg";
-import xalqaro from "../../assets/images/xalqaro.jpg";
+import fizika from "../../assets/images/fak_fizika.jpg";
+import boshlan from "../../assets/images/fak_boshlan.jpg";
+import uzbek from "../../assets/images/fak_uzbek.jpg";
+import ped from "../../assets/images/fak_ped.jpg";
+import jismon from "../../assets/images/fak_jismoniy.jpg";
+import maktab from "../../assets/images/fak_maktabgacha.jpg";
+import milliy from "../../assets/images/fak_milliy.jpg";
+import xorijiy from "../../assets/images/fak_xorijiy.jpg";
+import tabiiy from "../../assets/images/fak_tabiiy.jpg";
+import rus from "../../assets/images/fak-rus.jpg";
+import tarix from "../../assets/images/fak_tarix.jpg";
 import { Link } from "react-router-dom";
 import { useEffect, useMemo, useState } from "react";
 import { IoIosArrowForward } from "react-icons/io";
@@ -22,132 +29,132 @@ const FakultetlarCom = () => {
         value: "fizika-matematika",
         lavozim: "Fizika va matemtika fakulteti dekani",
         ism: "Poʻlatov Sharifjon Yigitaliyevich",
-        tel: "+998 99 998 00 57",
+        tel: " +998 97 725 75 63",
         mutahasisligi: "Texnika fanlari doktori, dotsent.",
         qabul: "Dushanba-shanba, 10:00 - 18:00",
         vazifasi:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum consequuntur earum labore dolorum? Quo, facilis. Unde a accusantium dignissimos mollitia, blanditiis eveniet modi deleniti! Obcaecati vel quia perferendis possimus eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. At, fugiat tenetur. Architecto labore officia reprehenderit quaerat laborum, dolorum veritatis animi repudiandae sint cupiditate enim minus modi quo? Iure, maxime earum",
-        img: raqamliImg,
+        img: fizika,
       },
       {
         label: "Boshlang'ich ta'lim",
         value: "boshlangich",
         lavozim: "Boshlang'ich ta'lim fakulteti dekani",
         ism: "Jamoliddinova Dilnoza Mirxojiddinovna",
-        tel: "+998 91 323 04 77",
+        tel: "+998 90 556 55 81",
         mutahasisligi: "Professor, Filoligiya fanlari doktori (DcS)",
         qabul: "Dushanba-shanba, 10:00 - 18:00",
         vazifasi:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum consequuntur earum labore dolorum? Quo, facilis. Unde a accusantium dignissimos mollitia, blanditiis eveniet modi deleniti! Obcaecati vel quia perferendis possimus eligendi.",
-        img: oquv,
+        img: boshlan,
       },
       {
         label: "O'zbek tili va adabiyoti",
         value: "ozbek-tili",
         lavozim: "O'zbek tili va adabiyoti fakulteti dekani",
         ism: "Hasanova Barnoxon Yusupovna",
-        tel: "+998 97 334 25 52",
+        tel: "+998 91 202 44 40",
         mutahasisligi: "-",
         qabul: "Dushanba-shanba, 10:00 - 18:00",
         vazifasi:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum consequuntur earum labore dolorum? Quo, facilis. Unde a accusantium dignissimos mollitia, blanditiis eveniet modi deleniti! Obcaecati vel quia perferendis possimus eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. At, fugiat tenetur. Architecto labore officia reprehenderit quaerat laborum, dolorum veritatis animi repudiandae sint cupiditate enim minus modi quo? Iure, maxime earum",
-        img: muhandis,
+        img: uzbek,
       },
       {
         label: "Pedagogika va psixologiya",
         value: "pedagogika-psixologiya",
         lavozim: "Pedagogika va psixologiya fakulteti dekani",
         ism: "Erkaboyeva Nigora Shermatovna",
-        tel: "+998 94 495 00 93",
+        tel: "+998 91 140 67 69",
         mutahasisligi: "Pedagogika fanlar doctori, Professor",
         qabul: "Dushanba-shanba, 10:00 - 18:00",
         vazifasi:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum consequuntur earum labore dolorum? Quo, facilis. Unde a accusantium dignissimos mollitia, blanditiis eveniet modi deleniti! Obcaecati vel quia perferendis possimus eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. At, fugiat tenetur. Architecto labore officia reprehenderit quaerat laborum, dolorum veritatis animi repudiandae sint cupiditate enim minus modi quo? Iure, maxime earum",
-        img: xalqaro,
+        img: ped,
       },
       {
         label: "Jismoniy madaniyat",
         value: "jismoniy-madaniyat",
         lavozim: "Jismoniy madaniyat fakulteti dekani",
         ism: "Talipjanov Alijon Akramovich",
-        tel: "+998 97 309 86 68",
+        tel: "+998 90 556 91 32",
         mutahasisligi: "Pedagogika fanlar nomzodi, dotsent",
         qabul: "Dushanba-shanba, 10:00 - 18:00",
         vazifasi:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum consequuntur earum labore dolorum? Quo, facilis. Unde a accusantium dignissimos mollitia, blanditiis eveniet modi deleniti!",
-        img: xalqaro,
+        img: jismon,
       },
       {
         label: "Maktabgacha ta'lim",
         value: "MaktabgachaTalim",
         lavozim: "Maktabgacha ta'lim fakulteti dekani",
         ism: "Abdullayev Oybek Qurbonovich",
-        tel: "+998 97 309 86 68",
+        tel: "+998 91 143 29 81",
         mutahasisligi: "-",
         qabul: "Dushanba-shanba, 10:00 - 18:00",
         vazifasi:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum consequuntur earum labore dolorum? Quo, facilis. Unde a accusantium dignissimos mollitia, blanditiis eveniet modi deleniti! Obcaecati vel quia perferendis possimus eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. At, fugiat tenetur. Architecto labore officia reprehenderit quaerat",
-        img: xalqaro,
+        img: maktab,
       },
       {
         label: "Milliy hunarmandchilik va amaliy san'at",
         value: "MilliyHunarmand",
         lavozim: "Milliy hunarmandchilik va amaliy san'at fakulteti dekani",
         ism: "Umrzaqov Behzod Bo‘ronovich",
-        tel: "+998 91 201 98 73",
+        tel: "+998 91 203 48 84 ",
         mutahasisligi: "-",
         qabul: "Dushanba-shanba, 10:00 - 18:00",
         vazifasi:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum consequuntur earum labore dolorum? Quo, facilis. Unde a accusantium dignissimos mollitia, blanditiis eveniet modi deleniti! Obcaecati vel quia perferendis possimus eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. At, fugiat tenetur. Architecto labore officia reprehenderit quaerat laborum, dolorum veritatis animi repudiandae sint cupiditate enim minus modi quo? Iure, maxime earum Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum consequuntur earum labore dolorum? Quo, facilis. Unde a accusantium dignissimos mollitia, blanditiis eveniet modi deleniti! Obcaecati vel quia perferendis possimus eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. At, fugiat tenetur. Architecto labore officia reprehenderit quaerat laborum, dolorum veritatis animi repudiandae sint cupiditate enim minus modi quo? Iure, maxime earum",
-        img: xalqaro,
+        img: milliy,
       },
       {
         label: "Xorijiy tillar",
         value: "XorijiyTillar",
         lavozim: "Xorijiy tillar fakulteti dekani",
         ism: "Karimova Vasila Vahobovna",
-        tel: "+998 94 440 83 31",
+        tel: "+998 91 145 99 00",
         mutahasisligi: "Filologiya fanlari bo‘yicha falsafa doktori (PhD)",
         qabul: "Dushanba-shanba, 10:00 - 18:00",
         vazifasi:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum consequuntur earum labore dolorum? Quo, facilis. Unde a accusantium dignissimos mollitia, blanditiis eveniet modi deleniti! Obcaecati vel quia perferendis possimus eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. At, fugiat tenetur. Architecto labore officia reprehenderit quaerat laborum, dolorum veritatis animi repudiandae sint cupiditate enim minus modi quo? Iure, maxime earum Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum consequuntur earum labore dolorum? Quo, facilis. Unde a accusantium dignissimos mollitia, blanditiis eveniet modi deleniti! Obcaecati vel quia perferendis possimus eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. At, fugiat tenetur. Architecto labore officia reprehenderit quaerat laborum, dolorum veritatis animi repudiandae sint cupiditate enim minus modi quo? Iure, maxime earum",
-        img: xalqaro,
+        img: xorijiy,
       },
       {
         label: "Tabiiy tillar",
         value: "TabiiyFanlar",
         lavozim: "Tabiiy tillar fakulteti dekani",
         ism: "Xo‘jayev Vaxobjon Umarovich",
-        tel: "+998 94 440 83 31",
+        tel: "+998 91 141 87 00",
         mutahasisligi: "Kimyo fanlari doktori, professor.",
         qabul: "Dushanba-shanba, 10:00 - 18:00",
         vazifasi:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum consequuntur earum labore dolorum? Quo, facilis. Unde a accusantium dignissimos mollitia, blanditiis eveniet modi deleniti!",
-        img: xalqaro,
+        img: tabiiy,
       },
       {
         label: "Rus tili va adabiyoti",
         value: "RusTili",
         lavozim: "Rus tili va adabiyoti fakulteti dekani",
         ism: "Rasulova Matlubaxon Xamzayevna",
-        tel: "+998 94 440 83 31",
+        tel: "+998 99 872 05 07",
         mutahasisligi: "Filologiya fanlari nomzodi, dotsent",
         qabul: "Dushanba-shanba, 10:00 - 18:00",
         vazifasi:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum consequuntur earum labore dolorum? Quo, facilis. Unde a accusantium dignissimos mollitia, blanditiis eveniet modi deleniti! Obcaecati vel quia perferendis possimus eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. At, fugiat tenetur. Architecto labore officia reprehenderit quaerat la",
-        img: xalqaro,
+        img: rus,
       },
       {
         label: "Tarix",
         value: "Tarix",
         lavozim: "Tarix fakulteti dekani",
         ism: "O‘rinboyev Davlatjon Baxtiyorovich",
-        tel: "+998 94 440 83 31",
+        tel: "+998 91 688 65 00",
         mutahasisligi: "Falsafa fanlari bo‘yicha falsafa doktori (PhD)",
         qabul: "Dushanba-shanba, 10:00 - 18:00",
         vazifasi:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum consequuntur earum labore dolorum? Quo, facilis. Unde a accusantium dignissimos mollitia, blanditiis eveniet modi deleniti! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum consequuntur earum labore dolorum? Quo, facilis. Unde a accusantium dignissimos mollitia, blanditiis eveniet modi deleniti! Obcaecati vel quia perferendis possimus eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. At, fugiat tenetur. Architecto labore officia reprehenderit quaerat laborum, dolorum veritatis animi repudiandae sint cupiditate enim minus modi quo? Iure, maxime earum",
-        img: xalqaro,
+        img: tarix,
       },
     ],
     []
@@ -279,7 +286,7 @@ const FakultetlarCom = () => {
                         </div>
                         <div className="flex flex-col justify-between xl:py-5 xl:mr-5">
                           <div className="px-3">
-                            <h3 className="block mb-2 font-sans text-lg xl:text-2xl antialiased font-semibold leading-snug tracking-normal text-[#004269] text-center lg:text-start">
+                            <h3 className="block mb-2 font-sans text-lg xl:text-2xl antialiased font-semibold leading-snug tracking-normal text-[#004269] text-center lg:text-start max-w-[450px]">
                               {lavozim}
                             </h3>
                             <h4 className="block mb-2 font-sans text-lg xl:text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900 text-center lg:text-start">
