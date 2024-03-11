0f0463b5c30e95627d93e5d02924a174e1c9d1b8
nodirbek27 <nodirjon0927@gmail.com>
2024-03-11 11:39:48 +0500

    carousel++

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -4,16 +4,6 @@ import mainVid from "../../assets/video/asosiy.mp4";
 const Carousel = () => {
   const mainRef = useRef(null);
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     if (mainRef.current) {
-  //       mainRef.current.play();
-  //     }
-  //   }, 2000);
-
-  //   return () => clearTimeout(timer);
-  // }, []);
-
   return (
     <div className="relative">
       <video
@@ -23,6 +13,7 @@ const Carousel = () => {
         src={mainVid}
         autoPlay
         loop
+        muted
       ></video>
       <div className="absolute top-0 left-0 w-full h-full z-0 bg-black opacity-35"></div>
     </div>
