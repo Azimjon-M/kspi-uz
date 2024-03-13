d181f8c76fd86017e78520dede1d2a73f2b9ae0c
nodirbek27 <nodirjon0927@gmail.com>
2024-03-13 09:09:37 +0500

    update++

--- src/components/NewsCard/index.jsx
+++ src/components/NewsCard/index.jsx
@@ -1,25 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
-import axios from "axios";
 import Breadcrumb from "../Breadcrumb";
 
 const NewsCard = () => {
-  const [news, setNews] = useState(null);
-
-  useEffect(() => {
-    const fetchNews = async () => {
-      try {
-        const response = await axios.get(
-          "https://api.kspi.uz/v1/yangilik/yangilik/"
-        );
-        setNews(response.data);
-      } catch (error) {
-        console.error("Error fetching news:", error);
-      }
-    };
-    fetchNews();
-  }, []);
   const { id } = useParams();
   const [newsData, setNewsData] = useState({});
 
@@ -44,6 +27,10 @@ const NewsCard = () => {
       </div>
 
       <div className="p-10 xl:px-0 md:grid md:grid-cols-1 lg:grid-cols-4 gap-10 max-w-7xl mx-auto">
+        {/* qoshimcha */}
+        <div></div>
+
+        {/* NEWS */}
         <div className="col-span-3 ...">
           <h2 className="text-xl text-[#004269] xl:text-2xl font-bold text-center mb-5">
             {newsData.title}
@@ -55,48 +42,6 @@ const NewsCard = () => {
             </p>
             <span>{newsData && newsData.body && newsData.body.slice(1)}</span>
           </div>
-        </div>
-        <div className="...">
-          {news &&
-            news.map((item, idx) => (
-              <Link to={`/yangiliklar/${item.id}`} key={idx}>
-                {item && (
-                  <div className="group/item hover:cursor-pointer">
-                    <div className="flex rounded-lg dark:bg-gray-800 shadow-md hover:shadow-lg flex-col group/edit">
-                      <div className="flex items-center mb-3 relative overflow-hidden">
-                        <img
-                          className="w-full rounded group-hover/item:scale-105 ease-in duration-300 ..."
-                          src={item.rasm}
-                          alt="Sunset in the mountains"
-                        />
-                        <div className="absolute top-0 left-3 h-12 w-12 bg-[#802323] text-center flex flex-col text-sm p-1 rounded-b-md">
-                          {item.sana && (
-                            <span className="text-white">
-                              {item.sana.slice(8, 10)}.{item.sana.slice(5, 7)}
-                            </span>
-                          )}
-                          {item.sana && (
-                            <span className="text-white">
-                              {item.sana.slice(0, 4)}
-                            </span>
-                          )}
-                        </div>
-                        <div className="absolute bottom-0 border-[#004269] border-2 w-10 group-hover/edit:w-full ... ease-in duration-300 ..."></div>
-                      </div>
-                      {/* News title */}
-                      <div className="flex flex-col justify-between flex-grow px-2">
-                        <h2 className="leading-relaxed font-bold line-clamp-3 xl:line-clamp-2 text-base text-[#004269] text-center dark:text-gray-300 line">
-                          {item.title}
-                        </h2>
-                        <div className="flex justify-center items-center">
-                          <div className="border-4 bg-[#004269] w-10 my-5"></div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                )}
-              </Link>
-            ))}
         </div>
       </div>
     </div>
