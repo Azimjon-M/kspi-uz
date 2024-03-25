71c7601854be1596f8732215da1f0c45e12c3475
Orifxon3595 <orifxontuychiyev@gmail.com>
2024-03-25 17:08:27 +0500

    update

--- src/components/ElonBatafsilCom/index.jsx
+++ src/components/ElonBatafsilCom/index.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import rrr from "../../assets/images/call.jpg";
 
 function ElonBatafsilCom() {
   return (
-    <div>ElonBatafsilCom</div>
-  )
+    <div>
+      <div className="bg bg-yellow-950 text-white py-10">
+        <div className="max-w-7xl mx-auto">
+          <p className="text-lg uppercase">Ishlash</p>
+          <h1 className="text-4xl">
+            Sent-Lorens va do'stlar: xayrli juma liturgik ijrosi
+          </h1>
+        </div>
+      </div>
+      <div className="max-w-7xl mx-auto">
+        <div>
+          <div>
+            <img src={rrr} className="" alt="" />
+            <div></div>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
 
-export default ElonBatafsilCom
+export default ElonBatafsilCom;
