4281c46250af086db83bc1845a75298e3db8de9b
Orifxon3595 <orifxontuychiyev@gmail.com>
2024-03-29 16:20:46 +0500

    update

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,12 +1,14 @@
-import React, { useRef } from "react";
-import mainVid from "../../assets/video/asosiy.mp4";
+import React from "react";
+import rasm from "../../assets/images/kspi-img-1.jpg"
+// import mainVid from "../../assets/video/asosiy.mp4";
 
 const Carousel = () => {
-  const mainRef = useRef(null);
+  // const mainRef = useRef(null);
 
   return (
     <div className="relative">
-      <video
+      <img src={rasm} alt="" />
+      {/* <video
         ref={mainRef}
         id="mainVideo"
         className="h-[50vh] md:h-[80vh] lg:h-[100vh] object-cover object-center w-full -z-1"
@@ -14,7 +16,7 @@ const Carousel = () => {
         autoPlay
         loop
         muted
-      ></video>
+      ></video> */}
       <div className="absolute top-0 left-0 w-full h-full z-0 bg-black opacity-35"></div>
     </div>
   );
