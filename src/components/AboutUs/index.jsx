4db1e96af7431c955a2f2be2bc8b8baa8f31ae7e
Orifxon3595 <orifxontuychiyev@gmail.com>
2024-04-05 16:32:44 +0500

    update

--- src/components/AboutUs/index.jsx
+++ src/components/AboutUs/index.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Typewriter } from "react-simple-typewriter";
+// import { Typewriter } from "react-simple-typewriter";
 import TextTranslate from "../TextTranslate";
-
 
 function AboutUs() {
   return (
@@ -14,11 +13,12 @@ function AboutUs() {
       </div>
       <div className="px-2 md:px-10 xl:px-0">
         <p className="text-xl md:text-[2rem] font-light font_source md:leading-[3.5rem] text-center my-10">
-          <Typewriter
+          <TextTranslate id="bizHaqimizda" />
+          {/* <Typewriter
             words={[]}
             typeSpeed={50}
             delaySpeed={1000}
-          />
+          /> */}
         </p>
       </div>
       <div className="flex justify-center">
@@ -26,7 +26,7 @@ function AboutUs() {
           to="/institut-haqida"
           className="text-lg md:text-2xl bg-slate-100 text-cyan-900 font-bold active:border border-slate-100 px-10 md:px-28 py-2 md:py-4 rounded-xl"
         >
-          Biz haqidas
+          <TextTranslate id="bizHaqimizdaKoproq" />
         </Link>
       </div>
     </div>
