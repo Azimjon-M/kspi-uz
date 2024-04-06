499f76d46e5afdf84b6c735cf599d7473216a514
Nodirbek Nurmamatov <103112982+nodirbek27@users.noreply.github.com>
2024-04-06 12:19:39 +0500

    carousel++

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,5 +1,4 @@
 import React, {useRef} from "react";
-// import rasm from "../../assets/images/kspi-img-1.jpg"
 import mainVid from "../../assets/video/asosiy.mp4";
 
 const Carousel = () => {
@@ -16,7 +15,7 @@ const Carousel = () => {
         loop
         muted
       ></video>
-      <div className="absolute top-0 left-0 w-full h-full z-0 bg-black opacity-35"></div>
+      <div className="absolute top-0 left-0 w-full h-full z-0 bg-gradient-to-b from-zinc-900 opacity-35"></div>
     </div>
   );
 };
