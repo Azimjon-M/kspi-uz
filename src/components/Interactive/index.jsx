ddba066c1115bff9a857927a311ef4fb549340ad
Orifxon3595 <orifxontuychiyev@gmail.com>
2024-04-18 19:32:45 +0500

    update

--- src/components/Interactive/index.jsx
+++ src/components/Interactive/index.jsx
@@ -13,15 +13,15 @@ function Interactive() {
   return (
     <div>
       <div className="max-w-7xl mx-auto px-6">
-        <div className="relative -top-5 z-2 py-2 mb-20 bg-white rounded-lg shadow-md">
+        <div className="relative -top-12 z-2 py-2 mb-20 bg-[#004269] rounded-lg shadow-lg">
           <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
             <li className="w-full">
               <Link
                 to="/qabulxona"
                 className="flex flex-col justify-center items-center group md:my-9 my-3 sm:border-r border-[#ebebeb] text-center"
               >
-                <RiComputerLine className="text-[40px] md:text-[50px] text-[#a3a1a2] group-hover:scale-125 group-hover:text-[#004269] transition-all duration-200 ease-linear" />
-                <h2 className="uppercase text-xs md:text-base mt-2 text-[#004269] font-bold">
+                <RiComputerLine className="text-[40px] md:text-[50px] text-slate-300 group-hover:scale-125 group-hover:text-white transition-all duration-200 ease-linear" />
+                <h2 className="uppercase text-xs md:text-base mt-2 text-slate-200 font-bold">
                   <TextTranslate id="intVertualQabulxona" />
                 </h2>
               </Link>
@@ -32,8 +32,8 @@ function Interactive() {
                 target="blank"
                 className="flex flex-col justify-center items-center group md:my-9 my-3 w-full md:border-r border-[#ebebeb] text-center"
               >
-                <GiBookshelf className="text-[40px] md:text-[50px] text-[#a3a1a2] group-hover:scale-125 group-hover:text-[#004269] transition-all duration-200 ease-linear" />
-                <h2 className="uppercase text-xs md:text-base mt-2 text-[#004269] font-bold">
+                <GiBookshelf className="text-[40px] md:text-[50px] text-slate-300 group-hover:scale-125 group-hover:text-white transition-all duration-200 ease-linear" />
+                <h2 className="uppercase text-xs md:text-base mt-2 text-slate-200 font-bold">
                   <TextTranslate id="intEKutubxona" />
                 </h2>
               </Link>
@@ -43,8 +43,8 @@ function Interactive() {
                 to="http://moodle.kspi.uz/doc/page/login.asp?_1707299150792"
                 className="flex flex-col justify-center items-center group md:my-9 my-3 w-full sm:border-r border-[#ebebeb] text-center"
               >
-                <PiChalkboardTeacherFill className="text-[40px] md:text-[50px] text-[#a3a1a2] group-hover:scale-125 group-hover:text-[#004269] transition-all duration-200 ease-linear" />
-                <h2 className="uppercase text-xs md:text-base mt-2 text-[#004269] font-bold">
+                <PiChalkboardTeacherFill className="text-[40px] md:text-[50px] text-slate-300 group-hover:scale-125 group-hover:text-white transition-all duration-200 ease-linear" />
+                <h2 className="uppercase text-xs md:text-base mt-2 text-slate-200 font-bold">
                   <TextTranslate id="intMasofaviyTalim" />
                 </h2>
               </Link>
@@ -54,8 +54,8 @@ function Interactive() {
                 to="/videomaruza"
                 className="flex flex-col justify-center items-center group md:my-9 my-3 w-full md:border-r border-[#ebebeb] text-center"
               >
-                <MdOutlineLiveTv className="text-[40px] md:text-[50px] text-[#a3a1a2] group-hover:scale-125 group-hover:text-[#004269] transition-all duration-200 ease-linear" />
-                <h2 className="uppercase text-xs md:text-base mt-2 text-[#004269] font-bold">
+                <MdOutlineLiveTv className="text-[40px] md:text-[50px] text-slate-300 group-hover:scale-125 group-hover:text-white transition-all duration-200 ease-linear" />
+                <h2 className="uppercase text-xs md:text-base mt-2 text-slate-200 font-bold">
                   <TextTranslate id="intVideoMaruzalar" />
                 </h2>
               </Link>
@@ -65,8 +65,8 @@ function Interactive() {
                 to="/qabul"
                 className="flex flex-col justify-center items-center group md:my-9 my-3 w-full sm:border-r border-[#ebebeb] text-center"
               >
-                <FaRegCalendarCheck className="text-[40px] md:text-[50px] text-[#a3a1a2] group-hover:scale-125 group-hover:text-[#004269] transition-all duration-200 ease-linear" />
-                <h2 className="uppercase text-xs md:text-base mt-2 text-[#004269] font-bold">
+                <FaRegCalendarCheck className="text-[40px] md:text-[50px] text-slate-300 group-hover:scale-125 group-hover:text-white transition-all duration-200 ease-linear" />
+                <h2 className="uppercase text-xs md:text-base mt-2 text-slate-200 font-bold">
                   <TextTranslate id="intQabul" />
                 </h2>
               </Link>
@@ -76,8 +76,8 @@ function Interactive() {
                 to="/kuzatish"
                 className="flex flex-col justify-center items-center group md:my-9 my-3 w-full text-center"
               >
-                <FaRegEye className="text-[40px] md:text-[50px] text-[#a3a1a2] group-hover:scale-125 group-hover:text-[#004269] transition-all duration-200 ease-linear" />
-                <h2 className="uppercase text-xs md:text-base mt-2 text-[#004269] font-bold">
+                <FaRegEye className="text-[40px] md:text-[50px] text-slate-300 group-hover:scale-125 group-hover:text-white transition-all duration-200 ease-linear" />
+                <h2 className="uppercase text-xs md:text-base mt-2 text-slate-200 font-bold">
                   <TextTranslate id="intImtixonlarniKuzatish" />
                 </h2>
               </Link>
