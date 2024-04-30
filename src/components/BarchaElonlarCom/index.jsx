c3f8d3b721c294ccc454b1854a817463cd85f649
Orifxon3595 <orifxontuychiyev@gmail.com>
2024-04-30 17:02:56 +0500

    update

--- src/components/BarchaElonlarCom/index.jsx
+++ src/components/BarchaElonlarCom/index.jsx
@@ -113,11 +113,11 @@ function BarchaElonlarCom() {
 
   return (
     <div className="bg-[#f4f4f4]">
-      <div className="max-w-5xl mx-auto md:min-h-[calc(100vh-565px)] lg:min-h-[calc(100vh-400px)] py-20">
+      <div className="max-w-5xl mx-auto md:min-h-[calc(100vh-565px)] lg:min-h-[calc(100vh-400px)] py-10 md:py-20">
         <h1 className="text-2xl md:text-4xl font-bold text-center text-[#004269]">
           Barcha elonlar
         </h1>
-        <div className="grid gap-5 md:mt-10 px-5">
+        <div className="grid gap-5 mt-10 px-5">
           {data &&
             data.map((item) => {
               return (
@@ -125,14 +125,14 @@ function BarchaElonlarCom() {
                   key={item.id}
                   className="flex p-4 md:p-5 shadow-md bg-white hover:-translate-y-2 duration-150 hover:shadow-2xl"
                 >
-                  <div className="lg:w-1/5 h-52 hidden md:block">
+                  <div className="md:w-1/5 h-52 hidden md:block">
                     <img
                       src={item.rasm}
                       className="w-full h-52 object-cover object-center"
                       alt=""
                     />
                   </div>
-                  <div className="md:pl-5 lg:w-4/5">
+                  <div className="md:pl-5 md:w-4/5">
                     <h3 className="text-base uppercase font-semibold text-red-800">
                       {item && item[`field_${Lang}`]}
                     </h3>
