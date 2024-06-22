876a3536452300e2751123edf8b9c9be241cd813
Nodirbek Nurmamatov <103112982+nodirbek27@users.noreply.github.com>
2024-06-22 10:36:13 +0500

    del video

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,5 +1,6 @@
 import React, {useRef} from "react";
-import mainVid from "../../assets/video/asosiy.mp4";
+// import mainVid from "../../assets/video/asosiy.mp4";
+import asosiy from "../../assets/images/StudentsHouse.jpg"
 
 const Carousel = () => {
   const mainRef = useRef(null);
@@ -7,7 +8,8 @@ const Carousel = () => {
 
   return (
     <div className="relative">
-      <video
+      <img src={asosiy} alt="" />
+      {/* <video
         ref={mainRef}
         id="mainVideo"
         className="h-[50vh] md:h-[80vh] lg:h-[100vh] object-cover object-center w-full -z-1"
@@ -15,7 +17,7 @@ const Carousel = () => {
         autoPlay
         loop
         muted
-      ></video>
+      ></video> */}
       <div className="absolute top-0 left-0 w-full h-full z-0 bg-gradient-to-b from-zinc-900 opacity-35"></div>
     </div>
   );
